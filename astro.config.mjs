a17d5e8b29e130b2189b98fd4f7891c316331c15
devoncats <jivasquezr2021@gmail.com>
2024-06-09 21:56:10 -0500

    Create hero section base design

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
     defaultLocale: 'en',
     locales: ['en', 'es'],
     routing: {
-      prefixDefaultLocale: 'en'
+      prefixDefaultLocale: false,
     }
   }
 });
