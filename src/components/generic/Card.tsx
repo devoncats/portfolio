f52ca4a7ac6df5c767e3f21ff4eaf8666b2a4150
devoncats <jivasquezr2021@gmail.com>
2024-06-15 14:25:54 -0500

    Added react and make the skeleton for skill cards

---
+++ src/components/generic/Card.tsx
@@ -0,0 +1,38 @@
+import ReactIcon from "@/icons/react"
+import TailwindIcon from "@/icons/tailwind"
+import HTMLIcon from "@/icons/html"
+import CSSIcon from "@/icons/css"
+import JavascriptIcon from "@/icons/javascript"
+import JavaIcon from "@/icons/java"
+import PostgresqlIcon from "@/icons/postgresql"
+import AstroIcon from "@/icons/astro"
+import GitIcon from "@/icons/git"
+
+interface Props {
+    skill: string
+}
+
+const iconMap: { [key: string]: React.ReactNode } = {
+    React: <ReactIcon />,
+    Tailwind: <TailwindIcon />,
+    HTML: <HTMLIcon />,
+    CSS: <CSSIcon />,
+    JavaScript: <JavascriptIcon />,
+    Java: <JavaIcon />,
+    Postgresql: <PostgresqlIcon />,
+    Astro: <AstroIcon />,
+    Git: <GitIcon />
+}
+
+const SkillCard: React.FC<Props> = ({ skill }) => {
+    const Icon = iconMap[skill] || null
+
+    return (
+        <div className="flex gap-4 px-8 py-2 items-center border bg-white rounded-lg">
+            {Icon}
+            {skill}
+        </div>
+    )
+}
+
+export default SkillCard
